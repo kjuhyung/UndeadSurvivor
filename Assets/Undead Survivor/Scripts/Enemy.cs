f3f3783f5501375151a11e63645c4ba16d466bcc
kim ju hyung <kim52116@naver.com>
2023-08-11 11:06:56 +0900

    몬스터 보완

--- Assets/Undead Survivor/Scripts/Enemy.cs
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -7,7 +7,8 @@ public class Enemy : MonoBehaviour
     // 속도,목표,생존여부 를 위한 변수 선언
     public float speed;
     public Rigidbody2D target;
-    bool isLive;
+
+    bool isLive = true;
 
     // 선언
     Rigidbody2D rigid;
@@ -22,11 +23,19 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (!isLive)
+            return;      
+
+        
         // 위치차이를 구해서 타겟방향으로 이동
         Vector2 dirVec = target.position - rigid.position;
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
         // 물리 속도가 이동에 영향을 주지 않도록 속도를 제거
         rigid.velocity = Vector2.zero;
+    }
+    void LateUpdate()
+    {
+        spriter.flipX = target.position.x < rigid.position.x;
     }
 }
