fe1f2bb3893c4509de6acce561d8e7ec9c3bb9e2
kim ju hyung <kim52116@naver.com>
2023-08-25 19:27:31 +0900

    총알 발사

--- Assets/Undead Survivor/Scripts/Bullet.cs
+++ Assets/Undead Survivor/Scripts/Bullet.cs
@@ -7,13 +7,37 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per)
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(float damage, int per, Vector3 dir)
     {
         // this = 해당 클래스의 변수로 접근
         this.damage = damage;
         this.per = per;
 
+        if (per > -1)
+        {
+            rigid.velocity = dir *15f; // 무한관통이 아니면 속도지정
+        }               
     }
-        
-    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 부딪힌게 적이 아니거나 무한관통이라면 반환
+        if (!collision.CompareTag("Enemy") || per == -1)
+            return;
+        per--; // 관통력 줄어들기
+
+        if (per == -1)
+        {
+            rigid.velocity = Vector2.zero; // 물리 속도 초기화
+            gameObject.SetActive(false); // 비활성화
+        }
+    }
+
+
 }
--- Assets/Undead Survivor/Scripts/Weapon.cs
+++ Assets/Undead Survivor/Scripts/Weapon.cs
@@ -40,11 +40,11 @@ public class Weapon : MonoBehaviour
                 break;
         }
 
-        //// ..Test code
-        //if (Input.GetButtonDown("Jump"))
-        //{
-        //    LevelUp(20, 5);
-        //}
+        // ..Test code
+        if (Input.GetButtonDown("Jump"))
+        {
+            LevelUp(10, 1);
+        }
 
     }
     
@@ -94,7 +94,7 @@ public class Weapon : MonoBehaviour
             Vector3 rotVec = Vector3.forward * 360 * index / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 1.5f, Space.World);
-            bullet.GetComponent<Bullet>().Init(damage, -1); // -1, 무한관통=근접무기
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // -1, 무한관통=근접무기
         }
     }
     void Fire()
@@ -102,7 +102,15 @@ public class Weapon : MonoBehaviour
         // 스캐너에 가장 가까운 타겟이 있으면 총알을 생성하는 로직
         if (!player.scanner.nearestTarget)
             return;
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized; // 현재 방향은 유지하고 크기를 1로 변환
+
         Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
-        bullet.position = transform.position;
+        bullet.position = transform.position; // 현재 위치에 총알 생성
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        // 지정된 축을 중심으로 목표를 향해 회전하는 함수
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
+
     }
 }
