0abda3d52a95e9598099bba763fca7783c77b629
kim ju hyung <kim52116@naver.com>
2023-08-31 12:49:22 +0900

    UI 기초설정
    캔버스 추가
    HUD.cs 생성

---
+++ Assets/Undead Survivor/Scripts/HUD.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType { Exp, Level, Kill, Time, Health }
+    public InfoType type;
+    // 열거형 생성, 열거형의 변수명 type 생성
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    void LateUpdate()
+    {
+        switch(type)
+        {
+            case InfoType.Exp:
+
+                break;
+            case InfoType.Level:
+
+                break;
+            case InfoType.Kill:
+                break;
+
+            case InfoType.Time:
+
+                break;
+            case InfoType.Health:
+
+                break;
+        }
+    }
+}
